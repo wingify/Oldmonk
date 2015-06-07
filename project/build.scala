bc806631a16b353f0fe1ece0b316b9455fcdb0b7
Chris Stucchio <stucchio@gmail.com>
2015-06-07 13:18:16 -0400

    unnecessary dependencies

--- project/build.scala
+++ project/build.scala
@@ -51,49 +51,13 @@ object ApplicationBuild extends Build {
   )
 
   object Dependencies {
-
-    val snowplowRawEvent = "com.snowplowanalytics" %  "snowplow-thrift-raw-event" % "0.1.0"
-    val collectorPayload = "com.snowplowanalytics" %  "collector-payload-1"       % "0.0.0"
-    val snowplowCollector = "com.snowplowanalytics" %% "snowplow-collector"       % "0.3.0.VWO"
-    val snowplow = Seq(snowplowRawEvent, collectorPayload, snowplowCollector)
-
-    val scalaz           = "org.scalaz"            %% "scalaz-core"               % "7.1.1"
-    val apacheCommonsCodec = "commons-codec"       % "commons-codec"              % "1.10"
-    val apacheCommonsMath = "org.apache.commons"   % "commons-math3"              % "3.5"
-
+    val scalaz           = "org.scalaz"            %% "scalaz-core"               % "7.1.2"
     val guava            = "com.google.guava"      % "guava"                      % "18.0"
     val spire            = "org.spire-math"        %% "spire"                     % "0.9.1"
 
-    val akkaVersion             = "2.3.9"
-    val akkaActor        = "com.typesafe.akka"     %% "akka-actor"                % akkaVersion
-    val akkaSlf4j        = "com.typesafe.akka"     %% "akka-slf4j"                % akkaVersion
-    val akkaStream       = "com.typesafe.akka"     %% "akka-stream-experimental"  % "1.0-M5"
-    val akka = Seq(akkaActor, akkaSlf4j, akkaStream)
-
-    val sprayVersion     = "1.3.2"
-    val sprayCan         = "io.spray"              %% "spray-can"                 % sprayVersion
-    val sprayRouting     = "io.spray"              %% "spray-routing"             % sprayVersion
-    val sprayJson        = "io.spray"              %%  "spray-json"               % "1.3.1"
-    val sprayCache       = "io.spray"              %% "spray-caching"              % sprayVersion
-    val sprayJsonLenses  = "net.virtual-void"      %%  "json-lenses"              % "0.6.0"
-
-    val spray = Seq(sprayCan, sprayRouting, sprayJson, sprayJsonLenses, sprayCache)
-
-    val logback          = "ch.qos.logback"        %  "logback-classic"           % "1.0.13"
-
-    val kafkaClients     = "org.apache.kafka"      %  "kafka-clients"             % "0.8.2.0"
-    val kafkaCore        = "org.apache.kafka"      %%  "kafka"                    % "0.8.2.0" //We should make this go away once the client consumer works
-    val kafka = Seq(kafkaClients, kafkaCore)
-
-    val metrics          = "io.dropwizard.metrics" %  "metrics-core"              % "3.1.0"
-    val metricsGraphite  = "io.dropwizard.metrics" %  "metrics-graphite"          % "3.1.0"
-    val instrumentation = Seq(metrics, metricsGraphite)
-
-    val thrift          = "org.apache.thrift"      %  "libthrift"                 % "0.9.1"  % "compile"
-
     val scalaCheck       = "org.scalacheck"        %% "scalacheck"                % "1.12.2" % "test"
 
-    val all = Seq(scalaz, logback, apacheCommonsCodec, apacheCommonsMath, guava, spire, thrift, scalaCheck) ++ spray ++ akka ++ snowplow ++ kafka ++ instrumentation
+    val all = Seq(scalaz, guava, spire, scalaCheck)
   }
 
   val myResolvers = Seq(
