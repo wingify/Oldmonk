e3b4e26ee6d8c5a7e6cc360ab7de8588a9a7af3f
Chris Stucchio <stucchio@gmail.com>
2015-07-20 14:21:55 -0400

    Added InvertedProbabilisticMultiMap, which uses bloom filters to handle mutable Map[K,V]-like objects where the number of V values is low.

---
+++ src/main/scala/com/vwo/oldmonk/datastructures/InvertedProbabilisticMultiMap.scala
@@ -0,0 +1,46 @@
+package com.vwo.oldmonk.datastructures
+
+import com.google.common.hash._
+
+class InvertedProbabilisticMultiMap[K,V](expectedInsertions: Int = 1024*1024, falsePositiveProbability: Double = 1e-12)(implicit keyFunnel: Funnel[K]) {
+  /*
+   * Use cases: You'll have a LOT of keys, but very few values. If you have K keys and V values, then the
+   * complexity of this class's get and set operations are O(V * log(K))
+   * There is also a finite probability of returning the wrong result.
+   *
+   * Due to the use of bloom filters, it is best to avoid inputting "null-like" objects.
+   * For example, an empty string, or a sequence of integers which are all zero.
+   * For reasons I can't figure out, they cause this class to return weirdo results.
+   */
+
+  private def newBloomFilter: BloomFilter[K] = BloomFilter.create[K](keyFunnel, expectedInsertions, falsePositiveProbability)
+
+  private val allValues: scala.collection.mutable.Set[V] = new scala.collection.mutable.HashSet[V]()
+  private val filterMap: scala.collection.mutable.Buffer[(V,BloomFilter[K])]  = new scala.collection.mutable.ArrayBuffer[(V,BloomFilter[K])]()
+
+  private def getFilter(value: V): Option[BloomFilter[K]] = filterMap.find(kv => kv._1 == value).map(_._2)
+  private var keysAdded: Long = 0
+
+  def add(key: K, value: V): Unit = {
+    keysAdded += 1
+    filterMap.find(kv => kv._1 == value).fold({
+      val bf = newBloomFilter
+      bf.put(key)
+      filterMap += (value -> bf)
+      ()
+    })(kv => {
+      kv._2.put(key)
+      ()
+    })
+  }
+
+  def fpp = filterMap.map(kv => kv._2.expectedFpp).max
+
+  def get(key: K): List[V] = filterMap.flatMap(kv => {
+    if (kv._2.mightContain(key)) {
+      List(kv._1)
+    } else {
+      List()
+    }
+  }).toList
+}
---
+++ src/test/scala/com/vwo/oldmonk/datastructures/InvertedProbabilisticMultiMapSpec.scala
@@ -0,0 +1,53 @@
+package com.vwo.oldmonk.datastructures
+
+import org.scalacheck._
+import org.scalacheck.Arbitrary.arbitrary
+import Prop._
+
+import scalaz._
+import Scalaz._
+
+import com.google.common.hash.{Funnel, PrimitiveSink}
+
+class InvertedProbabilisticMultiMapSpec extends Properties("InvertedProbabilisticMultiMap") {
+
+  implicit val stringFunnel = new Funnel[String] {
+    def funnel(data: String, into: PrimitiveSink) = into.putBytes(data.getBytes)
+  }
+
+  property("add and remove") = forAll( (k: String, v: Long) => {
+    val mp = new InvertedProbabilisticMultiMap[String,Long](1024, 1e-8)
+    mp.add(k,v)
+    mp.get(k).map(_ === v) === List(true)
+  })
+
+  property("add and remove many") = forAll( (dataExtra: Map[String,Long]) => {
+    val mp = new InvertedProbabilisticMultiMap[String,Long](1024, 1e-16)
+    val data = dataExtra.toList.filter(kv => kv._1.size > 0)
+    data.map(kv => mp.add(kv._1, kv._2) )
+    data.forall(kv => {
+      val getResult = mp.get(kv._1)
+      if (getResult.size > 1) {
+        println(getResult)
+        println(data.filter(kv => getResult.toSet.contains(kv._2)))
+      }
+      (getResult.headOption === Some(kv._2)) && getResult.tail.isEmpty
+    })
+  })
+
+  property("values not added are missing") = forAll( (k: String, k2: String, v: Long) => {
+    val mp = new InvertedProbabilisticMultiMap[String,Long](1024*1024, 1e-8)
+    mp.add(k,v)
+    (k != k2) ==> mp.get(k2).isEmpty
+  })
+
+  property("values not added are missing many") = forAll( (data: Map[String,Long], missing: Set[String]) => {
+    val mp = new InvertedProbabilisticMultiMap[String,Long](1024*1024, 1e-8)
+    data.toList.map(kv => mp.add(kv._1, kv._2) )
+    val reallyMissing = missing -- data.keySet // Need to remove
+    reallyMissing.forall( k => mp.get(k).isEmpty)
+  })
+
+
+
+}
