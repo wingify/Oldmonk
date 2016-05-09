102dd8e8a81b9aebba20a702cb8da01009df59da
Chris Stucchio@wingify <chris@wingify.com>
2016-05-09 07:51:44 -0400

    Added timed rotating bloom filter

--- src/main/scala/com/vwo/oldmonk/datastructures/RotatingBloomFilter.scala
+++ src/main/scala/com/vwo/oldmonk/datastructures/RotatingBloomFilter.scala
@@ -4,9 +4,15 @@ import com.google.common.hash._
 import com.google.common.base.Predicate
 
 class RotatingBloomFilter[A](insertionsBeforeRotate: Int = 1024*1024, falsePositiveProbability: Double = 1e-12)(implicit funnel: Funnel[A]) extends Predicate[A] {
-  private var mainFilter = BloomFilter.create[A](funnel, insertionsBeforeRotate, falsePositiveProbability)
-  private var oldFilter = BloomFilter.create[A](funnel, insertionsBeforeRotate, falsePositiveProbability)
-  private var numInsertions: Int = 0
+  /*
+   * A pair of bloom filters, once one fills up it's rotated into the second position
+   * and a new one is created. So basically it's a bloom filter but applied to a rolling
+   * window.
+   */
+
+  protected var mainFilter = BloomFilter.create[A](funnel, insertionsBeforeRotate, falsePositiveProbability)
+  protected var oldFilter = BloomFilter.create[A](funnel, insertionsBeforeRotate, falsePositiveProbability)
+  protected var numInsertions: Int = 0
 
   def apply(a: A) = mightContain(a)
 
@@ -16,9 +22,50 @@ class RotatingBloomFilter[A](insertionsBeforeRotate: Int = 1024*1024, falsePosit
     mainFilter.put(a)
     numInsertions += 1
     if (numInsertions > insertionsBeforeRotate) {
-      oldFilter = mainFilter
-      mainFilter = BloomFilter.create[A](funnel, insertionsBeforeRotate, falsePositiveProbability)
-      numInsertions = 0
+      rotate
     }
+  }
+
+  def rotate = {
+    oldFilter = mainFilter
+    mainFilter = BloomFilter.create[A](funnel, insertionsBeforeRotate, falsePositiveProbability)
+    numInsertions = 0
+  }
+
+  def clear = {
+    rotate
+    rotate
+  }
+}
+
+class TimedRotatingBloomFilter[A](insertionsBeforeRotate: Int = 1024*1024, falsePositiveProbability: Double = 1e-12, rotateTimeMillis: Long = 1000*60*60*24)(implicit funnel: Funnel[A]) extends RotatingBloomFilter(insertionsBeforeRotate, falsePositiveProbability)(funnel) {
+  /*
+   * Similar functionality as for rotating bloom filter, but also rotations happen at fixed intervals.
+   */
+  private var lastRotateTime = System.currentTimeMillis
+
+  override def mightContain(a: A) = {
+    rotateByTime
+    super.mightContain(a)
+  }
+
+  private def rotateByTime = {
+    if (System.currentTimeMillis > lastRotateTime + rotateTimeMillis) {
+      rotate
+    }
+  }
+
+  override def add(a: A) = {
+    mainFilter.put(a)
+    numInsertions += 1
+    if (numInsertions > insertionsBeforeRotate) {
+      rotate
+    }
+    rotateByTime
+  }
+
+  override def rotate = {
+    super.rotate
+    lastRotateTime = System.currentTimeMillis
   }
 }
