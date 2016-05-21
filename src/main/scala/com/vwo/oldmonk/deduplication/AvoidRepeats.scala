b5d7660d1540156ab4eb7a948abbe442fe462ea9
Chris Stucchio@wingify <chris@wingify.com>
2016-05-21 19:59:02 -0400

    Added repeat avoider based on semiconcurrent bloom fiter

--- src/main/scala/com/vwo/oldmonk/deduplication/AvoidRepeats.scala
+++ src/main/scala/com/vwo/oldmonk/deduplication/AvoidRepeats.scala
@@ -1,6 +1,6 @@
 package com.vwo.oldmonk.deduplication
 
-import com.vwo.oldmonk.datastructures.RotatingBloomFilter
+import com.vwo.oldmonk.datastructures.{RotatingBloomFilter, SemiConcurrentBloomFilter}
 
 import com.google.common.cache._
 import java.util.concurrent.TimeUnit
@@ -85,6 +85,56 @@ trait ProvidesAvoidRepeats {
       def apply(key: A, otherArgs: B) = {
         val fromCache = cache.getIfPresent(key)
         if (fromCache == null) {
+          f(key, otherArgs)
+          Some(ConcreteDelayedIdempotent(key)) : Option[IdempotentMarker[A]]
+        } else {
+          None
+        }
+      }
+    }
+
+    def concurrentProbabilistic[A, B](f: (A,B) => Unit, filterSize: Int = 1024*1024, falsePositiveProbability: Double = 1e-12, insertionsBeforeCompact: Int = 1024)(implicit funnel: Funnel[A]): DelayedIdempotentEffect[A,B] = new DelayedIdempotentEffect[A,B] {
+      import java.util.concurrent.atomic._
+
+      private def newFilter = new SemiConcurrentBloomFilter[A](filterSize*2, falsePositiveProbability, insertionsBeforeCompact)(funnel)
+
+      private val filter1 = new AtomicReference[SemiConcurrentBloomFilter[A]](newFilter)
+      private val filter2 = new AtomicReference[SemiConcurrentBloomFilter[A]](newFilter)
+      private val insertions = new AtomicLong(0)
+
+      private def rotate = {
+        val f1 = filter1.get()
+        filter2.set(f1)
+        filter1.set(newFilter)
+      }
+
+      private val self = this
+      private var requiresRotate = new AtomicBoolean(false)
+
+      private case class ConcreteDelayedIdempotent(key: A) extends IdempotentMarker[A] {
+        def apply() = {
+          val f1 = filter1.get()
+          f1.add(key)
+          val inserted = insertions.incrementAndGet()
+          if (inserted % filterSize == 0) {
+            requiresRotate.set(true)
+            self.synchronized {
+              val needsRotate = requiresRotate.get()
+              if (needsRotate) {
+                rotate
+                insertions.set(0)
+                requiresRotate.set(false)
+              }
+            }
+          }
+        }
+      }
+
+      private def mightContain(key: A) = (!filter1.get().mightContain(key)) && (!filter2.get().mightContain(key))
+
+      def apply(key: A, otherArgs: B) = {
+        val runAction = mightContain(key)
+        if (runAction) {
           f(key, otherArgs)
           Some(ConcreteDelayedIdempotent(key)) : Option[IdempotentMarker[A]]
         } else {
